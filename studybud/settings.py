ccce4f477c77c003c85ae31b3369e089cba1b4b8
Anib <saliv.mah@gmail.com>
2022-06-23 21:20:03 +0545

    ready for heroku

--- studybud/settings.py
+++ studybud/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8gfi69uo1=@^dghmrrlyi8wjvafy#ts^es(cy%wl67xv(@#bx#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['chikuwaapp.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
